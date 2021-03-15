51103cc8ddb6113103e469130aa08dc2329d8391
Onyiee <elizabethonyemowo.agbo@gmail.com>
2021-03-15 09:48:13 +0100

    Book class

---
+++ src/com/library/Book.java
@@ -0,0 +1,23 @@
+package com.library;
+
+public class Book {
+ private String title;
+ private Author author;
+
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setAuthor(Author author) {
+        this.author = author;
+    }
+
+    public Author getAuthor() {
+        return author;
+    }
+}
